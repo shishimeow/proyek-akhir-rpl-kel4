eebf37897a25017c839dcd8f4d2e88f42288f385
annadia dwina <88170371+annadiadwinahaqiqi@users.noreply.github.com>
2023-06-04 00:26:22 +0700

    nested comments

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -5,16 +5,18 @@
   <input type="hidden" name="user_id" value="{{ $review->user_id }}">
   <input type="hidden" name="type" value="{{ $type }}">
   <div class="col-12">
-    <textarea class="col-8 d-flex" rows="5" name="comment" id="comment" wrap="hard"></textarea>
+    <textarea class="col-8 d-flex form-control" rows="5" name="comment" id="comment" wrap="hard"></textarea>
   </div>
-  <button type="button" class="btn btn-primary" name="action" onclick="cancelFormComment({{ $loop->index }})">cancel</button>
-  <button type="submit" class="btn btn-primary" name="action" value="reply">reply</button>
+  <div class="pb-3 pt-3">
+  <button type="button" class="btn btn-primary" name="action" onclick="cancelFormComment({{ $loop->index }})">Cancel</button>
+  <button type="submit" class="btn btn-primary" name="action" value="reply">Reply</button>
+  </div>
   </form>
 
 <a class="comment__card-footer" data-bs-toggle="collapse" href="#multiCollapseExample{{$loop->index}}" role="button" aria-expanded="false" aria-controls="multiCollapseExample{{$loop->index}}">
   @if( $review->comments()->where('rev_id', $review->id)->whereNull('parent_id')->count() > 0)
-      {{ $review->comments()->where('rev_id', $review->id)->whereNull('parent_id')->count() }} Reply
-  @else Reply
+      {{ $review->comments()->where('rev_id', $review->id)->whereNull('parent_id')->count() }} Replies
+  @else Replies
   @endif
 </a>
 
@@ -28,34 +30,50 @@
               <div class="col-12">
                 <strong>{{ $comment->user->username }}</strong>
                 {{ \Carbon\Carbon::parse($comment->created_at)->locale('id_ID')->isoFormat('D MMMM YYYY') }}
-                <p>{{ $comment->comment }}</p>
+                
               </div>
               
-              @if($comment->user_id == auth()->user()->id)
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                  <i class="bx bx-dots-vertical-rounded"></i>
-                </button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" data-bs-toggle="collapse" href="#editComment{{ $comment->id }}"
-                    ><i class="bx bx-edit-alt me-2"></i> Edit</a
-                  >
-                  <a class="dropdown-item" href="#" onclick="deleteComment({{ $comment->id }})"
-                    ><i class="bx bx-trash me-2"></i> Delete</a
-                  >
-                </div>
-              </div>
+              @if($comment->user_id == auth()->user()->id)   
+              <div class="col">
+                          <div class="d-flex justify-content-end">
+                              <button type="button" class="btn dropdown-toggle hide-arrow text-end " data-bs-toggle="dropdown">
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                              </button>
+                                <div class="dropdown-menu">
+                                  <a class="dropdown-item" data-bs-toggle="collapse" href="#editComment{{ $comment->id }}"
+                                    ><i class="bx bx-edit-alt me-2"></i> Edit</a
+                                  >
+                                  <a class="dropdown-item" href="#" onclick="deleteComment({{ $comment->id }})"
+                                    ><i class="bx bx-trash me-2"></i> Delete</a
+                                  >
+                                </div>
+                            </div>
+                                                                                                                                 
+                            <div class="col-12 fs-5 pb-3">
+                              <p>{{ $comment->comment }}</p>
+                            </div>
+                            </div>
+                        
 
+                            </div>
+
+                            
+                            
+
+              </div>
+              
               <form class="collapse multi-collapse" action="{{ route('update.add') }}" method="POST" id="editComment{{ $comment->id }}">
                   @method('put')
                   @csrf
                   <input type="hidden" name="comment_id" value="{{ $comment->id }}">
                   <input type="hidden" name="type" value="{{ $comment->type }}">
                   <div class="col-12">
-                      <textarea class="col-8 d-flex" rows="5" name="comment" id="comment" wrap="hard" required>{{ $comment->comment }}</textarea>
+                      <textarea class="col-8 d-flex form-control" rows="5" name="comment" id="comment" wrap="hard" required>{{ $comment->comment }}</textarea>
                   </div>
-                  <button type="button" class="btn btn-primary" name="action" onclick="cancelFormReply({{ $comment->id }})">cancel</button>
-                  <button type="submit" class="btn btn-primary" name="action" value="save">save</button>
+                  <div class="pb-3 pt-3">
+                  <button type="button" class="btn btn-primary" name="action" onclick="cancelFormReply({{ $comment->id }})">Cancel</button>
+                  <button type="submit" class="btn btn-primary" name="action" value="save">Save</button>
+                  </div>
               </form>
               <form action="{{ route('delete.add') }}" method="POST" id="delCom{{ $comment->id }}">
                   @method('delete')
@@ -63,8 +81,11 @@
                   <input type="hidden" name="comment_id" value="{{ $comment->id }}">
               </form>
               @endif
-
+              <div class="pt-3">
               <a data-bs-toggle="collapse" href="#replyComment{{$loop->index}}" aria-expanded="false" aria-controls="replyComment{{$loop->index}}">Reply</a>
+              </div>
+
+              
               <form class="collapse multi-collapse" action="{{ route('reply.add') }}" method="POST" id="replyComment{{$loop->index}}">
                   @csrf
                   <input type="hidden" name="rev_id" value="{{ $comment->id }}">
@@ -72,16 +93,18 @@
                   <input type="hidden" name="comment_id" value="{{ $comment->id }}">
                   <input type="hidden" name="type" value="{{ $comment->type }}">
                   <div class="col-12">
-                  <textarea class="col-8 d-flex" rows="5" name="comment" id="comment" wrap="hard"></textarea>
+                  <textarea class="col-8 d-flex form-control" rows="5" name="comment" id="comment" wrap="hard"></textarea>
                   </div>
-                  <button type="button" class="btn btn-primary" name="action" onclick="cancelReplyComment({{ $loop->index }})">cancel</button>
-                  <button type="submit" class="btn btn-primary" name="action" value="reply">reply</button>
-              </form>
+                  <div class="pb-3 pt-3">
+                  <button type="button" class="btn btn-primary" name="action" onclick="cancelReplyComment({{ $loop->index }})">Cancel</button>
+                  <button type="submit" class="btn btn-primary" name="action" value="reply">Reply</button>
+                  </div>
+                </form>
 
               <a class="comment__card-footer" data-bs-toggle="collapse" href="#replyCollapse{{$comment->id}}" role="button" aria-expanded="false" aria-controls="replyCollapse{{$comment->id}}">
                 @if( $comment->where('parent_id', $comment->id)->count() > 0)
-                {{ $comment->where('parent_id', $comment->id)->count() }} Reply
-                @else Reply
+                {{ $comment->where('parent_id', $comment->id)->count() }} Replies
+                @else Replies
                 @endif
             </a>
 
