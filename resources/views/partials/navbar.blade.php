a46030faf96938449dc6b473bd29fe45e08af0e3
annadia dwina <88170371+annadiadwinahaqiqi@users.noreply.github.com>
2023-06-03 12:11:26 +0700

    Update navbar.blade.php
    update navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,8 +6,9 @@
           <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
             <div class="app-brand demo">
               <div class="img-container">
-                <img src="{{ asset('img/assets/avatars/Logo.png') }}" alt="Logo" width="35%">
-                <span class="app-brand-text demo menu-text fw-bolder ms-2">I-Course IPB</span>
+              <img src="{{ asset('img/assets/avatars/Logo.png') }}" alt="Logo" width="40%">
+                <span class="app-brand-text demo fw-bolder ms-0" style="text-transform: capitalize; font-size: 22px ">I-Course Center</span>
+
               </div>  
                 
   
@@ -18,7 +19,7 @@
   
             <div class="menu-inner-shadow"></div>
   
-            <ul class="menu-inner py-1">
+            <ul class="menu-inner py-4">
               <!-- Components -->
               
               <!-- Cards -->
